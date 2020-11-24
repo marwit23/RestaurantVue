69c71ea1a70c495d0ae766fbb2323e04d1962e17
marcin <marwit23@gmail.com>
2020-11-24 11:57:57 +0100

    Development1

--- src/usercomponents/todo/AddTodoModal.vue
+++ src/usercomponents/todo/AddTodoModal.vue
@@ -1,127 +1,125 @@
 <template>
-        <b-modal id="add-todo-modal" ref="add-todo-modal" okTitle="Submit" okVariant="warning" @ok="onSubmit" @hidden="resetModal">
-        <b-container class="container-sm">
+  <b-modal
+    id="add-todo-modal"
+    ref="add-todo-modal"
+    okTitle="Submit"
+    okVariant="warning"
+    @ok="onSubmit"
+    @hidden="resetModal"
+  >
+    <b-container class="container-sm">
+      <h5 class="mb-4">New Recipe</h5>
 
-            <h5 class="mb-4">New Recipe</h5>
+      <b-form @submit.prevent="onSubmit" class="border-bottom pb-2">
+        <b-row class="my-2">
+          <b-col sm="6">
+            <b-form-input
+              v-model="formData.dishName"
+              placeholder="Enter dish name"
+              onfocus="this.placeholder = ''"
+              class="mb-2 mr-sm-2"
+            >
+            </b-form-input>
+          </b-col>
+        </b-row>
 
-            <b-form @submit.prevent="onSubmit" class="border-bottom pb-2">
-
-              <b-row class="my-2">
-                <b-col sm="6">
-                  <b-form-input
-                  v-model="formData.dishName"
-                  placeholder="Enter dish name"
-                  onfocus="this.placeholder = ''" 
-                  class="mb-2 mr-sm-2">
-                  </b-form-input>
-                </b-col>
-              </b-row>
-
-              <div v-for="ingredient in formData.ingredients" :key="ingredient.dishIngredientId">
-
-              <b-form-row>
-                
-              <b-col>
-              <b-form-select 
-                v-model="ingredient.ingredientName"
-                :options="ingredientOptions"
-                class="mb-2 mr-sm-2">
+        <div v-for="ingredient in formData.ingredients" :key="ingredient.dishIngredientId">
+          <b-form-row>
+            <b-col>
+              <b-form-select v-model="ingredient.ingredientName" :options="ingredientOptions" class="mb-2 mr-sm-2">
               </b-form-select>
-              </b-col>
+            </b-col>
 
-              <b-col>
-              <b-form-input 
-              v-model="ingredient.quantityGrams"
-              placeholder="Quantity in grams"
-              onfocus="this.placeholder = ''" 
-              class="mb-2 mr-sm-2">
+            <b-col>
+              <b-form-input
+                v-model="ingredient.quantityGrams"
+                placeholder="Quantity in grams"
+                onfocus="this.placeholder = ''"
+                class="mb-2 mr-sm-2"
+              >
               </b-form-input>
-              </b-col>
+            </b-col>
 
-              <b-col>
-              <b-button 
+            <b-col>
+              <b-button
                 v-if="ingredient === formData.ingredients[formData.ingredients.length - 1]"
-                variant="primary" 
-                class="ml-4 mb-2 mr-sm-2" 
-                @click="addItem">+
+                variant="primary"
+                class="ml-4 mb-2 mr-sm-2"
+                @click="addItem"
+                >+
               </b-button>
-              <b-button 
-                v-if="ingredient === formData.ingredients[formData.ingredients.length - 1] &&
-                  ingredient !== formData.ingredients[0]" 
+              <b-button
+                v-if="
+                  ingredient === formData.ingredients[formData.ingredients.length - 1] &&
+                    ingredient !== formData.ingredients[0]
+                "
                 variant="secondary"
                 class="mb-2 mr-sm-2"
-                @click="deleteItem">-
+                @click="deleteItem"
+                >-
               </b-button>
-              </b-col>
+            </b-col>
+          </b-form-row>
+        </div>
+      </b-form>
 
-              </b-form-row>
-
-              </div>
-
-            </b-form>
-
-            <pre v-html="formData"/>
-
-        </b-container>
-    </b-modal>
+      <pre v-html="formData" />
+    </b-container>
+  </b-modal>
 </template>
 <script>
-import { mapActions, mapGetters } from "vuex"
-import router from '../../router'
+import { mapActions, mapGetters } from "vuex";
+import router from "../../router";
 
+export default {
+  name: "AddTodoModal",
+  data() {
+    return {
+      formData: {
+        dishName: "",
+        ingredients: [
+          {
+            ingredientName: "",
+            quantityGrams: "",
+          },
+        ],
+      },
+    };
+  },
 
-export default { 
-    name: "AddTodoModal",
-    data() {
-        return {
-            formData: {
-              dishName: '',
-              ingredients: [
-                {
-                  ingredientName: '',
-                  quantityGrams: ''
-                }
-              ]
-            }
-        }
+  computed: {
+    ...mapGetters(["allRecipes", "allIngredients"]),
+
+    ingredientOptions() {
+      const tempArray1 = this.allIngredients.map((ingredient) => ingredient.ingredientName);
+      return tempArray1;
     },
+  },
 
-    computed: {
-        ...mapGetters(["allRecipes", "allIngredients"]),
+  methods: {
+    ...mapActions(["addRecipe"]),
 
-        ingredientOptions(){
-            const tempArray1 = this.allIngredients.map(ingredient => ingredient.ingredientName)
-            return tempArray1
-        },
+    onSubmit() {
+      this.addRecipe(this.formData);
     },
-
-    methods: {
-        ...mapActions(["addRecipe"]),
-
-        onSubmit() {
-            this.addRecipe(this.formData)
-
-        },
-        addItem(){
-          this.formData.ingredients.push({
-            ingredientName: '',
-            quantityGrams: ''
-          })
-        },
-        deleteItem(){
-          this.formData.ingredients.pop();
-        },
-        onCancel(){
-          router.push({ path : '/admin/recipes' })
-        },
-        resetModal() {
-            this.ingredient.ingredientName = ''
-            this.ingredient.ingredientCategory = ''
-            this.ingredient.shelfLife = ''
-        },
-    }
-
-}
+    addItem() {
+      this.formData.ingredients.push({
+        ingredientName: "",
+        quantityGrams: "",
+      });
+    },
+    deleteItem() {
+      this.formData.ingredients.pop();
+    },
+    onCancel() {
+      router.push({ path: "/admin/recipes" });
+    },
+    resetModal() {
+      this.ingredient.ingredientName = "";
+      this.ingredient.ingredientCategory = "";
+      this.ingredient.shelfLife = "";
+    },
+  },
+};
 </script>
-<style>
-</style>
+<style></style>
--- src/usercomponents/todo/Todos.vue
+++ src/usercomponents/todo/Todos.vue
@@ -1,147 +1,207 @@
 <template>
     <div class="container">
         <div class="row mt-4">
+            <b-col>
+                <b-button v-b-modal.add-todo-modal variant="warning ml-3 btn-lg font-size">+</b-button>
+            </b-col>
 
-        <b-col>
-        <b-button v-b-modal.add-todo-modal variant="warning ml-3 btn-lg font-size">+</b-button>
-        </b-col>
-
-        <b-col class="col-11">
-
-        <div 
-            class="card mb-2"
-            :class="[{'border-danger': todo.toDoStatus == 'OPENED'}]"
-            v-for="todo in allTodos" 
-            :key="todo.toDoDishId">
-          <div class="card-header">Order id: {{todo.toDoDishId}}</div>
-          <div class="card-body">
-            <b-row>
-                <b-col 
-                    class="font-weight-bold" 
-                    :class="[{disabled: todo.toDoStatus == 'COMPLETED'}]">{{todo.dishName}}
-                </b-col>
-                <b-col 
-                    class="text-center font-weight-bold"
-                    :class="[{disabled: todo.toDoStatus == 'COMPLETED'}]">
-                    {{todo.dishQuantity}}
-                    <b-button 
-                        variant="outline-secondary btn-sm ml-3" 
-                        :disabled="todo.toDoStatus == 'COMPLETED' "
-                        @click="incrementQuantity(todo)">+
-                    </b-button>
-                    <b-button 
-                        variant="outline-secondary btn-sm ml-2"
-                        :disabled="todo.toDoStatus == 'COMPLETED' || todo.dishQuantity == 1 " 
-                        @click="decrementQuantity(todo)">-
-                    </b-button>
-                </b-col>
-                <b-col 
-                    class="text-right mr-4"
-                    :class="[{redbold: todo.toDoStatus == 'OPENED'}, {green: todo.toDoStatus == 'COMPLETED'},]">{{todo.toDoStatus}}
-                </b-col>
-            </b-row>        
-          </div>
-          <div class="card-footer">
-            <b-row>
-                <b-col>
-                    <b-button 
-                        v-if="todo.toDoStatus !== 'COMPLETED' "
-                        variant="outline-secondary btn-sm" 
-                        :disabled="todo.toDoStatus == 'COMPLETED' ">Edit
-                    </b-button>
-                    <b-button 
-                        v-if="todo.toDoStatus !== 'COMPLETED' " 
-                        v-b-modal.modal-1 @click="setTempTodo(todo)"
-                        variant="outline-secondary btn-sm ml-2">Delete
-                    </b-button>
-                </b-col>
-                <b-col>
-                    <b-button 
-                        v-if="todo.toDoStatus == 'WAITING' " 
-                        variant="secondary mr-3 float-right" 
-                        @click="setOpened(todo)">Open
-                    </b-button>
-                    <div v-else-if="todo.toDoStatus == 'OPENED' " >
-                        <b-button 
-                            variant="danger mr-3 float-right" 
-                            @click="setCompleted(todo)">Complete
-                        </b-button> 
-                        <b-button 
-                            variant="outline-secondary mr-3 float-right" 
-                            @click="setWaiting(todo)">Cancel
-                        </b-button> 
+            <b-col class="col-11">
+                <transition-group name="list-complete">
+                    <div
+                        class="card mb-2 list-complete-item"
+                        :class="[{ 'border-danger': todo.toDoStatus == 'OPENED' }]"
+                        v-for="todo in todosSorted"
+                        :key="todo.toDoDishId"
+                    >
+                        <div class="card-header">Order id: {{ todo.toDoDishId }}</div>
+                        <div class="card-body">
+                            <b-row>
+                                <b-col
+                                    class="font-weight-bold"
+                                    :class="[
+                                        {
+                                            disabled: todo.toDoStatus == 'COMPLETED',
+                                        },
+                                    ]"
+                                    >{{ todo.dishName }}
+                                </b-col>
+                                <b-col
+                                    class="text-center font-weight-bold"
+                                    :class="[
+                                        {
+                                            disabled: todo.toDoStatus == 'COMPLETED',
+                                        },
+                                    ]"
+                                >
+                                    {{ todo.dishQuantity }}
+                                    <b-button
+                                        variant="outline-secondary btn-sm ml-3"
+                                        :disabled="todo.toDoStatus == 'COMPLETED'"
+                                        @click="incrementQuantity(todo)"
+                                        >+
+                                    </b-button>
+                                    <b-button
+                                        variant="outline-secondary btn-sm ml-2"
+                                        :disabled="todo.toDoStatus == 'COMPLETED' || todo.dishQuantity == 1"
+                                        @click="decrementQuantity(todo)"
+                                        >-
+                                    </b-button>
+                                </b-col>
+                                <b-col
+                                    class="text-right mr-4"
+                                    :class="[
+                                        { redbold: todo.toDoStatus == 'OPENED' },
+                                        { green: todo.toDoStatus == 'COMPLETED' },
+                                    ]"
+                                    >{{ todo.toDoStatus }}
+                                </b-col>
+                            </b-row>
+                        </div>
+                        <div class="card-footer">
+                            <b-row>
+                                <b-col>
+                                    <b-button
+                                        v-if="todo.toDoStatus !== 'COMPLETED'"
+                                        variant="outline-secondary btn-sm"
+                                        :disabled="todo.toDoStatus == 'COMPLETED'"
+                                        >Edit
+                                    </b-button>
+                                    <b-button
+                                        v-if="todo.toDoStatus !== 'COMPLETED'"
+                                        v-b-modal.modal-1
+                                        @click="setTempTodo(todo)"
+                                        variant="outline-secondary btn-sm ml-2"
+                                        >Delete
+                                    </b-button>
+                                </b-col>
+                                <b-col>
+                                    <b-button
+                                        v-if="todo.toDoStatus == 'WAITING'"
+                                        variant="secondary mr-3 float-right"
+                                        @click="setOpened(todo)"
+                                        >Open
+                                    </b-button>
+                                    <div v-else-if="todo.toDoStatus == 'OPENED'">
+                                        <b-button variant="danger mr-3 float-right" @click="setCompleted(todo)"
+                                            >Complete
+                                        </b-button>
+                                        <b-button variant="outline-secondary mr-3 float-right" @click="setWaiting(todo)"
+                                            >Cancel
+                                        </b-button>
+                                    </div>
+                                </b-col>
+                            </b-row>
+                        </div>
                     </div>
-                </b-col>
-            </b-row>
-               
-          </div>
-        </div>
+                </transition-group>
+            </b-col>
 
-        </b-col>
+            <b-modal id="modal-1" okVariant="danger" okTitle="Delete" @ok="deleteTodo(tempTodo.toDoDishId)">
+                <h5 class="mb-4">Confirm delete order ID: {{ tempTodo.dishName }}</h5>
+                <p class="my-4">
+                    Are you sure you want to delete this item? This action cannot be undone.
+                </p>
+            </b-modal>
 
-        <b-modal id="modal-1" okVariant="danger" okTitle="Delete" @ok="deleteTodo(tempTodo.toDoDishId)">
-            <h5 class="mb-4">Confirm delete order ID: {{tempTodo.dishName}}</h5>
-            <p class="my-4">Are you sure you want to delete this item? This action cannot be undone.</p>
-        </b-modal>
-
-        <AddTodoModal />
-
+            <AddTodoModal />
         </div>
     </div>
 </template>
 <script>
-import { mapGetters, mapActions } from 'vuex'
-import AddTodoModal from './AddTodoModal.vue'
+import { mapGetters, mapActions } from "vuex";
+import AddTodoModal from "./AddTodoModal.vue";
+// import TodoCardList from "./TodoCardList.vue";
 
 export default {
-    name: 'Todos',
-    
+    name: "Todos",
+
     data() {
         return {
             tempTodo: {},
-            isEditable: false
-        }
+            isEditable: false,
+        };
     },
-     components: {
-        AddTodoModal
-
+    components: {
+        AddTodoModal,
+        // TodoCardList,
     },
 
     computed: {
         ...mapGetters(["allTodos"]),
 
-        sortedList(){
-            var tempSortedList = JSON.parse(JSON.stringify(this.allTodos))
-            tempSortedList.sort(this.propComparator(this.sortParam))
-            return tempSortedList
+        // todosOpened() {
+        //     var todosOpenedArray = this.allTodos.filter((todo) => todo.toDoStatus == "OPENED");
+        //     return todosOpenedArray;
+        // },
+
+        // todosWaiting() {
+        //     var todosWaitingArray = this.allTodos.filter((todo) => todo.toDoStatus == "WAITING");
+        //     return todosWaitingArray;
+        // },
+        // todosCompleted() {
+        //     var todosCompletedArray = this.allTodos.filter((todo) => todo.toDoStatus == "COMPLETED");
+        //     return todosCompletedArray;
+        // },
+        todosCopy() {
+            return JSON.parse(JSON.stringify(this.allTodos))
+        },
+        todosOpenedArray() {
+            return this.todosCopy.filter((todo) => todo.toDoStatus == "OPENED")
+        },
+        todosWaitingArray() {
+            return this.todosCopy.filter((todo) => todo.toDoStatus == "WAITING")
+        },
+        todosCompletedArray() {
+            return this.todosCopy.filter((todo) => todo.toDoStatus == "COMPLETED")
+        },
+        todosSorted() {
+            var tempSorted = this.todosOpenedArray.concat(this.todosWaitingArray, this.todosCompletedArray)
+            return tempSorted
         }
+
+
+        //     for (var todo in todosSorted) {
+        //         if (todo.toDoStatus == "OPENED") {
+        //             tempSortedList.splice(openedLength, 0, todo);
+        //         } else if (todo.toDoStatus == "WAITING") {
+        //             tempSortedList.splice(waitingLength, 1, todo);
+        //         } else if (todo.toDoStatus == "COMPLETED") {
+        //             tempSortedList.splice(completedLength, 1, todo);
+        //         }
+        //     }
+        //     console.log(tempSortedList)
+        //     return tempSortedList;
+        // },
     },
 
     methods: {
         ...mapActions(["getAllTodos", "editTodo", "deleteTodo"]),
 
         setTempTodo(todo) {
-            this.tempTodo = todo
-            console.log(this.tempTodo)
+            this.tempTodo = todo;
+            console.log(this.tempTodo);
         },
         setOpened(todo) {
-            todo.toDoStatus = 'OPENED'
+            todo.toDoStatus = "OPENED"
+            this.todosSorted.splice(this.todosSorted.indexOf(todo), 1)
+            this.todosSorted.splice(this.todosOpenedArray.length, 0, todo)
             this.editTodo(todo)
         },
         setCompleted(todo) {
-            todo.toDoStatus = 'COMPLETED'
+            todo.toDoStatus = "COMPLETED"
             this.editTodo(todo)
         },
         setWaiting(todo) {
-            todo.toDoStatus = 'WAITING'
+            todo.toDoStatus = "WAITING"
             this.editTodo(todo)
+
         },
         incrementQuantity(todo) {
-            todo.dishQuantity +=1
+            todo.dishQuantity += 1
             this.editTodo(todo)
         },
         decrementQuantity(todo) {
-            todo.dishQuantity -=1
+            todo.dishQuantity -= 1
             this.editTodo(todo)
         },
     },
@@ -149,11 +209,9 @@ export default {
     created() {
         this.getAllTodos();
     },
-
 };
 </script>
 <style>
-
 .redbold {
     color: red;
     font-weight: bold;
@@ -167,9 +225,15 @@ export default {
     color: gray;
 }
 
+.list-complete-item {
+  transition: all 1s;
+  display: block;
+}
+.list-complete-enter, .list-complete-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.list-complete-leave-active {
+  position: absolute;
+}
 </style>
-
-
-
-
-
